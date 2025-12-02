63760de1e9426d029c5255c543fa5d15a76109d6
Fred Bergman <fred@fbergman.se>
2025-12-02 21:36:49 +0100

    Add solutions for 2025 day 2

---
+++ 2025/02/part_1.rb
@@ -0,0 +1,9 @@
+input = File.read("input.txt")
+
+puts input.split(",").sum { |range_str|
+  range_start, range_end = range_str.split("-").map(&:to_i)
+
+  (range_start..range_end).sum { |num|
+    num.to_s.match(/^(\d+)\1$/) ? num : 0
+  }
+}
